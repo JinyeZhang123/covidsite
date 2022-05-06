ff098a9be5160a1fee764da3bf7cad56d10098bb
JinyeZhang123 <77717551+JinyeZhang123@users.noreply.github.com>
2022-05-06 16:41:10 -0400

    Delete covid site php Jinye Zhang directory

--- covid site php Jinye Zhang/connectdb.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $connection = new PDO('mysql:host=localhost; port=3306; dbname=coviddb', "root", "");
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
